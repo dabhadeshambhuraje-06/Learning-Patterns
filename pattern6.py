b455f15b5c2d04a85e14bd1e6b16429dd48f771d
Shambhuraje Dabhade <dabhadeshambhuraje@gmail.com>
2026-05-10 11:37:30 +0530

    added 3 more patterns

---
+++ pattern6.py
@@ -0,0 +1,17 @@
+#1
+#1 2 
+#1 2 3
+#1 2 3 4
+#1 2 3 4 5
+
+n=int(input())
+
+#outer loop for rows
+for i in range(1,n+1):
+    
+    #inner loop for columns
+    for j in range(1,i+1):
+        print(j,end=" ")
+
+    print()
+        
---
+++ pattern7.py
@@ -0,0 +1,15 @@
+#5 4 3 2 1
+#5 4 3 2
+#5 4 3 
+#5 4
+#5
+
+n = int(input())
+
+#to print no. of. rows
+for i in range(1,n+1):
+    for j in range(n,i-1,-1):
+        print(j,end=" ")
+    print()
+
+
